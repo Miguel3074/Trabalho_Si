048eab8c7651a60bc4f4c6610c02747b76296041
Miguel Brack <miguelanjosbrack@gmail.com>
2025-04-14 22:52:18 -0300

    Versao 1.1
    funcao pra criar grafico de custo x tempo e temperatura x tempo
    testes
    
    Signed-off-by: Miguel Brack <miguelanjosbrack@gmail.com>

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ struct Cidade
 };
 
 int i = 0;
+vector<double> historicoT;
+vector<double> historicoCusto;
 
 vector<Cidade> lerGrafoCSV(const string &nomeArquivo)
 {
@@ -107,13 +109,55 @@ vector<int> simulatedAnnealing(vector<int> rotaInicial, const vector<Cidade> &ci
             melhorRota = rotaAtual;
         }
 
-        //cout << "\nmelhor Custo: " << custoRota(melhorRota, cidades)<< "      Custo: " << custoRota(rotaAtual, cidades) <<"    T   "<< T;
+        // cout << "\nmelhor Custo: " << custoRota(melhorRota, cidades)<< "      Custo: " << custoRota(rotaAtual, cidades) <<"    T   "<< T;
+
+        cout << custoAtual << "\n ";
 
         T *= alpha;
         i++;
+        historicoT.push_back(T);
+        historicoCusto.push_back(custoAtual);
     }
 
     return melhorRota;
+}
+
+void gerarGraficoSVG(const vector<double> &dados, const string &nomeArquivo, const string &titulo, const string &eixoY)
+{
+    ofstream svg(nomeArquivo);
+    if (!svg.is_open())
+    {
+        cerr << "Erro ao criar SVG\n";
+        return;
+    }
+
+    int width = 1000, height = 400;
+    svg << "<svg xmlns='http://www.w3.org/2000/svg' width='" << width << "' height='" << height << "'>\n";
+    svg << "<style> .eixo { stroke: black; stroke-width: 1; } .linha { stroke: blue; stroke-width: 1; fill: none; } </style>\n";
+
+    svg << "<line x1='40' y1='" << height - 30 << "' x2='" << width - 10 << "' y2='" << height - 30 << "' class='eixo'/>\n"; // eixo x
+    svg << "<line x1='40' y1='10' x2='40' y2='" << height - 30 << "' class='eixo'/>\n";                                      // eixo y
+
+    svg << "<text x='" << width / 2 - 50 << "' y='20' font-size='14'>" << titulo << "</text>\n";
+    svg << "<text x='" << width / 2 << "' y='" << height - 5 << "' font-size='12'>tempo</text>\n";
+
+    double maxValor = *max_element(dados.begin(), dados.end());
+    double escalaY = (height - 40.0) / maxValor;
+    double passoX = (width - 60.0) / dados.size();
+
+    svg << "<polyline class='linha' points='";
+    for (size_t i = 0; i < dados.size(); ++i)
+    {
+        double x = 40 + i * passoX;
+        double y = height - 30 - dados[i] * escalaY;
+        svg << x << "," << y << " ";
+    }
+    svg << "'/>\n";
+
+    svg << "<text x='5' y='" << height / 2 << "' font-size='12' transform='rotate(-90, 15," << height / 2 << ")'>" << eixoY << "</text>\n";
+
+    svg << "</svg>\n";
+    svg.close();
 }
 
 void gerarSVG(const vector<Cidade> &cidades, const vector<int> &rota, const string &nomeArquivo) // funcao pra vizualizacao da melhor rota SVG, gpt q fez, entendi como funciona mt bem
@@ -203,28 +247,38 @@ int main()
 
     // atributos
     double T_inicial = 10000; // temperatura inicial  para cada 0 tbm aumenta o tempo de processamento mas n tanto quanto o alpha
-    double T_final = 0.00001;    // temperatura final   n entendi muito bem como funciona ainda, esse numero foi o gpt que deu quando perguntei sobre os atributos
-    double alpha = 0.9999;     // taxa de resfriamento (0,00001 eu acho q ta)  PARA CADA 9 NO FINAL AUMENTA MUITO O TEMPO DE PROCESSAMENTO MAS DA UMA RESPOSTA MELHOR, com 0.99999; consegui 18790.5 mas demorou uns 5-10 min
+    double T_final = 0.001; // temperatura final   n entendi muito bem como funciona ainda, esse numero foi o gpt que deu quando perguntei sobre os atributos
+    double alpha = 0.99999;    // taxa de resfriamento (0,00001 eu acho q ta)  PARA CADA 9 NO FINAL AUMENTA MUITO O TEMPO DE PROCESSAMENTO MAS DA UMA RESPOSTA MELHOR, com 0.99999; consegui 18790.5 mas demorou uns 5-10 min
 
-
+    // double T_inicial = 10000;
+    // double T_final = 0.00001;
+    // double alpha = 0.9999;  i = 20723256x e melhor custo 18242.2
 
     // double T_inicial = 10000; 
-    // double T_final = 0.00001;
-    // double alpha = 0.999999;  deixei um tempao rodando com esses e deu 18242.2
+    // double T_final = 0.001;
+    // double alpha = 0.999995;  i = 3223612x e melhor custo 18706.1
 
+    // double T_inicial = 10000; 
+    // double T_final = 0.001;
+    // double alpha = 0.9999; i = 161173x e melhor custo 25973.9
+
+    // double T_inicial = 10000; 
+    // double T_final = 0.001;
+    // double alpha = 0.99995; i = 322354x e melhor custo 21249.1
+
+    // double T_inicial = 10000; 
+    // double T_final = 0.001;
+    // double alpha = 0.99999; i = 1611802x e melhor custo 18359.5     ***** Até agora acho q foi o q mais valeu a pena em (i/custo), deu mais sorte q o de 3223612x
 
     vector<int> melhorRota = simulatedAnnealing(rota, cidades, T_inicial, T_final, alpha);
-
-    cout << "\nMelhor Rota Encontrada: ";
-    for (int i : melhorRota)
-    {
-        cout << i << " ";
-    }
+    
     cout << "\nCusto da Rota: " << custoRota(melhorRota, cidades) << "\n";
 
     gerarSVG(cidades, melhorRota, "rota_final.svg");
+    gerarGraficoSVG(historicoT, "grafico_temperatura.svg", "Temperatura ao longo do tempo", "Temperatura");
+    gerarGraficoSVG(historicoCusto, "grafico_custo.svg", "Custo da rota ao longo do tempo", "Custo");
 
-    cout << "i: " << i;
+    cout << "i: " << i << "\n ";
 
     return 0;
 }
